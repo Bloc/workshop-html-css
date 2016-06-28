61cab35c841eb0d550d1f96dfca739324dd36203
Stanley Idesis <stan@bloc.io>
2016-06-28 20:50:31 +0000

    Small Adjustments to the List Exercise

--- spec/007-lists.rb
+++ spec/007-lists.rb
@@ -1,11 +1,13 @@
 describe "index.html" do
   let(:index) { File.open("index.html", "rb").read }
 
-  it "has a ul tag" do
+  it "has a ul (unordered list) tag" do
     expect(index).to have_tag("ul")
   end
 
-  it "has 4 li tags" do
-    expect(index).to have_tag("li", count: 4)
+  it "has 4 li tags within the ul" do
+    expect(index).to have_tag("ul") do
+      with_tag "li", count: 4
+    end
   end
 end
