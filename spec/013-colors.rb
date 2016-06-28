ba16e207cd814375c37cd1cf74958f3567af949b
Stanley Idesis <stan@bloc.io>
2016-06-28 21:47:48 +0000

    Fixed the Colors Assignment

--- spec/013-colors.rb
+++ spec/013-colors.rb
@@ -6,18 +6,18 @@ describe "index.html" do
     p.load_file! "main.css"
     p
   end
-  
+
   context "body" do
     let(:body_rules) { parser.find_by_selector('body').join(' ').downcase }
-    
+
     it "has a lighter font weight" do
       expect(body_rules).to include('font-weight', 'lighter')
     end
-    
+
     it "has a 36px font size" do
       expect(body_rules).to include('font-size', '36px')
     end
-    
+
     it "has a #fff3e3 text color" do
       expect(body_rules).to include('color', '#fff3e3')
     end
@@ -26,12 +26,12 @@ describe "index.html" do
   context "links" do
     let(:a_rules) { parser.find_by_selector('a').join(' ').downcase }
 
-    it "has a #ffb985 text color" do
+    it "have a #ffb985 text color" do
       expect(a_rules).to include('color', '#ffb985')
     end
   end
 
-  context "headers" do
+  context "h1" do
     let(:h1_rules) { parser.find_by_selector('h1').join(' ').downcase }
 
     it "has a #ae7182 text color" do
@@ -42,7 +42,7 @@ describe "index.html" do
   context "table headers" do
     let(:th_rules) { parser.find_by_selector('th').join(' ').downcase }
 
-    it "has a #fff3e3 border color" do
+    it "have a #fff3e3 border color" do
       expect(th_rules).to include('border', '#fff3e3')
     end
   end
@@ -54,20 +54,13 @@ describe "index.html" do
       expect(table_rules).to include('border', '#262b53')
     end
   end
-  
-  context ".language" do
+
+  context "language" do
     let(:language_rules) { parser.find_by_selector('.language').join(' ').downcase }
 
     it "has a #262b53 background color" do
       expect(language_rules).to include('background', '#262b53')
     end
   end
-
-  context "#aloha" do
-    let(:aloha_rules) { parser.find_by_selector('#aloha').join(' ').downcase }
-
-    it "has a #6d5a7f background color" do
-      expect(aloha_rules).to include('background', '#6d5a7f')
-    end
-  end
+  
 end
